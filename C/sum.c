81cb5aadfeb78a0e36513cd3868e01d1836b145f
Nguyen Hong Quan <quannhpd09461@fpt.edu.vn>
2023-02-07 18:17:05 +0700

    New

---
+++ C/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Nhap a: ");
+    scanf("%d", &a);
+    printf("Nhap b: ");
+    scanf("%d", &b);
+    int c = a + b;
+    printf("KQL: %d ", c);
+    return 0;
+}
