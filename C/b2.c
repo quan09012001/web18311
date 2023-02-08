f72988e70731623cb1abc183a46d0266edbe3c87
Nguyen Hong Quan <quannhpd09461@fpt.edu.vn>
2023-02-08 16:01:04 +0700

    Nguyen Hong Quan

--- C/b2.c
+++ C/b2.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-
-int main(){
+// S(tron) = a * a * 3.14
+// S(vuong) = a * a
+// S(tam giac can) = S(vuong / 4)
+int main()
+{
     const int v = 4;
-    int a, b;
+    const float n = 3.14;
+    int a;
     printf("nhap a: ");
-    scacnf
+    scanf("%d", &a);
+    float St = a * a * n;
+    int Sv = a * a;
+    float Stg = (float)Sv / v;
+    printf("Dien tich hinh tron la: %.2f \n", St);
+    printf("Dien tich hinh Vuong la: %d \n", Sv);
+    printf("Dien tich hinh tam giac can la: %.2f", Stg);
+    return 0;
 }
